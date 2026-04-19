fcd940627a94164f72fe74f59a455f62e6397f46
Akanksha Muthe <akankshamuthe2625@gmail.com>
2026-04-19 19:36:27 +0530

    Configure frontend API base URL globally

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,8 @@
-const API = process.env.REACT_APP_API_URL || "";
+import axios from "axios";
+
+const API = (process.env.REACT_APP_API_URL || "").replace(/\/$/, "");
+
+axios.defaults.baseURL = API;
 
 export const apiUrl = (path) => `${API}${path}`;
 
